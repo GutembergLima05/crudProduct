88633fa7804735dc6a5a99759a697efd209a6a25
Gutemberg Lima <j.berglima05@gmail.com>
2023-12-28 12:45:45 -0300

    Added PostgreSQL in project

---
+++ crudProduct/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crudProduct
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseNpgsql("Server=tuffi.db.elephantsql.com;Port=5432;Database=htfoikvs;User Id=htfoikvs;Password=bdV1JHvvVMtx8p5S9C82mUiPH4W_Dbv5;");
+    }
+}
