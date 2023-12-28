175549206be4fae96879771c469ab15ce3900a7b
Gutemberg Lima <j.berglima05@gmail.com>
2023-12-28 13:35:51 -0300

    CRUD product created and finished

--- crudProduct/Program.cs
+++ crudProduct/Program.cs
@@ -25,6 +25,50 @@ app.UseHttpsRedirection();
 app.MapGet("/product", async (DataContext context) =>
  await context.Products.OrderBy(p => p.Id).ToListAsync());
 
+app.MapGet("/product/{id}", async (DataContext context, int id) =>
+await context.Products.FindAsync(id) is Product product ?
+Results.Ok(product) : Results.NotFound("Product not found."));
+
+app.MapPost("/product", async(DataContext context, Product product) =>
+{
+    if (string.IsNullOrWhiteSpace(product.Name))
+        return Results.BadRequest("The Name field is required.");
+
+    context.Products.Add(product);
+    await context.SaveChangesAsync();
+    return Results.Ok(product);
+});
+
+app.MapPut("/product/{id}", async(DataContext context, Product updateProduct, int id) =>
+{
+    var product = await context.Products.FindAsync(id);
+    if (product is null)
+        return Results.NotFound("Product not found.");
+
+    if (string.IsNullOrWhiteSpace(updateProduct.Name))
+        return Results.BadRequest("The Name field is required.");
+
+    product.Name = updateProduct.Name;
+    product.Description = updateProduct.Description;
+    product.Price = updateProduct.Price;
+    await context.SaveChangesAsync();
+
+    return Results.Ok(product);
+});
+
+app.MapDelete("/product/{id}", async (DataContext context, int id) =>
+{
+    var product = await context.Products.FindAsync(id);
+    if (product is null)
+        return Results.NotFound("Product not found.");
+
+    context.Products.Remove(product);
+    await context.SaveChangesAsync();
+
+    return Results.Ok(product);
+});
+
+
 app.Run();
 
 public class Product
