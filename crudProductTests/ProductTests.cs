d10ab61b712e94c710a2961e28873c5d80645a5b
Gutemberg Lima <j.berglima05@gmail.com>
2024-01-09 09:00:49 -0300

    Clean Code update

--- crudProductTests/ProductTests.cs
+++ crudProductTests/ProductTests.cs
@@ -1,10 +1,20 @@
+using crudProduct.Models;
+
 namespace crudProductTests;
 
 public class ProductTests
 {
+
+    private Product productTestes;
+
+    public ProductTests()
+    {
+        productTestes = new Product();
+    }
+
     [Fact]
     public void Test1()
     {
-
+        
     }
 }
